e641ded1e27e3477865b6357a299e510f12e93c7
Raveen <raveenanbarasan@gmail.com>
2020-11-07 12:12:46 +0530

    final

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.counter.value,
+  };
+  render() {
+    return (
+      <div>
+        {this.props.children}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          className="btn btn-secondary btn-sm m-2"
+          onClick={() => this.handleIncreament()}
+        >
+          Increment
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  handleIncreament() {
+    this.setState({ value: this.state.value + 1 });
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
---
+++ src/components/counters.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
+      { id: 4, value: 4 },
+    ],
+  };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+          >
+            <h4>counter #{counter.id} </h4>
+          </Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
